9833b58b47a466213f24ae78d2a9273220826907
havenJdev <havenjallen@gmail.com>
2022-02-04 09:48:44 -0700

    Add exercise 2.2 Date.java

---
+++ Exercise2.2/src/Date.java
@@ -0,0 +1,26 @@
+
+public class Date {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String day = "Friday";
+		String month = "February";
+		int date = 4;
+		int year = 2022;
+
+		System.out.println("Day: "+day);
+		System.out.println("Date: "+date);
+		System.out.println("Month: "+month);
+		System.out.println("Year: "+year);
+		System.out.println(day + ", " + month + " " + date + ", " + year);
+		System.out.print(day);
+		System.out.print(" ");
+		System.out.print(date);
+		System.out.print(" ");
+		System.out.print(month);
+		System.out.print(" ");
+		System.out.println(year);
+		
+	}
+
+}
