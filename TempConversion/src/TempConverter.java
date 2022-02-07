fb7a3b8fc7210822125294b151fdb302f52ab619
Haven <havenjallen@gmail.com>
2022-02-07 10:01:59 -0700

    user input for temperature converter

--- TempConversion/src/TempConverter.java
+++ TempConversion/src/TempConverter.java
@@ -1,18 +1,32 @@
+import java.util.Scanner;
 
 public class TempConverter {
 
 	public static void main(String[] args) {
-		double tempF = 25;
-		double tempC;
 		
-		// F -> C
-		tempC = (tempF - 32.0) * (5.0/9.0);
-		System.out.printf("Temp: %.2f\n", tempC);
+		double degF;
+		double degC;
+
+		Scanner in = new Scanner(System.in);
 		
-		// C -> F
-		tempC = 38.6;
-		tempF = tempC * (9.0/5.0) + 32;
-		System.out.printf("Temp: %.2f\n",tempF);
+		System.out.print("Deg F: ");
+		degF = in.nextDouble();
+		System.out.println("You input: " + degF);
+
+		double temp;
+		
+		temp = (degF - 32.0) * (5.0/9.0);
+		System.out.printf("Temp in degrees C: %.2f\n", temp);
+
+		System.out.print("Deg C: ");
+		degC = in.nextDouble();
+		System.out.println("You input: " + degC);
+
+		temp = degC * (9.0/5.0) + 32;
+		System.out.printf("Temp in degrees F: %.2f\n",temp);
+		
+		in.close();
+		
 	}
 
 }
