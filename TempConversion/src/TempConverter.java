e53a35f7a8e506cf9438af7127cbeffaf35391a2
havenJdev <havenjallen@gmail.com>
2022-02-09 09:34:31 -0700

    better userinput to tempconverter project

--- TempConversion/src/TempConverter.java
+++ TempConversion/src/TempConverter.java
@@ -4,29 +4,38 @@ public class TempConverter {
 
 	public static void main(String[] args) {
 		
-		double degF;
-		double degC;
-
+		double userValue;
+		String degreeType;
+		
 		Scanner in = new Scanner(System.in);
 		
-		System.out.print("Deg F: ");
-		degF = in.nextDouble();
-		System.out.println("You input: " + degF);
-
-		double temp;
+		System.out.print("Degree unit (F/C): ");
+		degreeType = in.nextLine();
 		
-		temp = (degF - 32.0) * (5.0/9.0);
-		System.out.printf("Temp in degrees C: %.2f\n", temp);
-
-		System.out.print("Deg C: ");
-		degC = in.nextDouble();
-		System.out.println("You input: " + degC);
-
-		temp = degC * (9.0/5.0) + 32;
-		System.out.printf("Temp in degrees F: %.2f\n",temp);
+		if ( degreeType.toLowerCase().equals("f") ) {
+			System.out.print("\nDegrees F: ");
+			userValue = in.nextDouble();
+			System.out.printf("Temp in degrees C: %.2f\n", fahrenheitToCelsius(userValue));
+		} else if ( degreeType.toLowerCase().equals("c") ) {
+			System.out.print("\nDegrees C: ");
+			userValue = in.nextDouble();
+			System.out.printf("Temp in degrees F: %.2f\n", celsiusToFahrenheit(userValue));
+		} else {
+			System.out.println("Something went wrong");
+		}
 		
 		in.close();
 		
+	}
+	
+	//My favorite part about java is how I don't have to prototype methods. God bless
+	public static double fahrenheitToCelsius(double degF) {
+		
+		return (degF - 32.0) * (5.0/9.0);
+	}
+	public static double celsiusToFahrenheit(double degC) {
+		
+		return degC * (9.0/5.0) + 32;
 	}
 
 }
